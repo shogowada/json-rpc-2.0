7cae89f6fc0bc9e5be14ab554f9abd296d4889ef
Shogo Wada <shogowada@users.noreply.github.com>
2021-11-21 20:47:38 -0600

    Remove unused code

--- src/internal.ts
+++ src/internal.ts
@@ -1,11 +1,1 @@
-export const createLogDeprecationWarning = (message: string) => {
-  let warned = false;
-  return () => {
-    if (!warned && process.env.NODE_ENV !== "production") {
-      console.warn(`Deprecation Warning (json-rpc-2.0): ${message}`);
-      warned = true;
-    }
-  };
-};
-
 export const DefaultErrorCode = 0;
